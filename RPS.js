89b8a3d6daa8fb3d4e1bc6c48cace448f3b09d12
Nguyen <Nguyennguyen.apr@gmail.com>
2022-05-07 18:35:53 -0700

    edit RPS.js to add playerPlay() function
    Instead of prompting the user input itself, game() would now
    call playerPlay(). playerPlay prompts user for input and returns
    the string, lowercased.

--- RPS.js
+++ RPS.js
@@ -5,16 +5,10 @@
 //     Returns 0 if draw or invalid input, 1 if payer wins, 2 if computer wins.
 // ============================================================================
 function playRound(player, computer) {
-    // Base case, Computer's error
-    if("error" === computer) {
-        console.log("invalid input from the computer. My bad");
-        return 0;
-    }
-
     // Create a dictionary and assign number to rps strings
     var dict = { "paper": 0, "scissor": 1, "rock":2 };
 
-    // If the input is valid
+    // If the inputs are valid
     if (player in dict) {
         // math. Basic but neat.
         var result = dict[player] - dict[computer];
@@ -61,8 +55,23 @@ function computerPlay() {
         default:
             break;
     }
-    // returns error if the answer cannot be generated.
-    return "error";
+    // returns rock if the answer cannot be generated.
+    return "rock";
+}
+
+
+
+// ============================================================================
+// This function prompts the user to play the game and gets the input.
+//     It takes no arguement
+//     Returns the user input string.
+// ============================================================================
+function playerPlay() {
+    // Prompt player for value
+    let player = window.prompt(
+                "Let's play for 5 rounds\nRock, paper, or scissor?");
+    // returns the lowercased input
+    return player.toLowerCase();
 }
 
 
@@ -80,16 +89,10 @@ function game() {
 
     // Iterate 5 times
     for (let i = 0; i < 5; i++) {
-        // Call computerPlay() to get computer's answer
-        let computer = computerPlay();
-        // Prompt player for input
-        let player = window.prompt(
-            "Let's play for 5 rounds\nRock, paper, or scissor?");
-        player = player.toLowerCase();
-
-        // Call playRound(), append player's score if the player wins,
+        // Call playRound(), which calls playerPlay() and computerPlay()
+        // append player's score if the player wins,
         // append computer's score if the computer wins
-        switch(playRound(player, computer)) {
+        switch(playRound(playerPlay(), computerPlay())) {
             case 1:
                 playerScore += 1;
                 break;
@@ -102,10 +105,10 @@ function game() {
     }
 
     // Anouncing total score
-    console.log("Player's score:   " + playerScore);
-    console.log("Computer's score: " + compScore);
+    console.log("Player's score:   " + playerScore + 
+                "\nComputer's score: " + compScore);
 
-    // Announcing winner, or if it's a draw
+    // Announcing the winner, or if it's a draw
     if(playerScore == compScore) {
         console.log("It's a draw!");
     }
