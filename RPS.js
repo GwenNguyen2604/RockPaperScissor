3a1e31a0a7b125cf527a6c1197eedc59a70f82b0
Nguyen <Nguyennguyen.apr@gmail.com>
2022-06-12 18:56:02 -0700

    Changes in the rock paper scissor files In HTML file:     - Add an empty div to be manipulated via DOM methods. In JavaScript file:     - Remove all window alerts and window prompts.     - Remove iteration in the game. The game only stops and       announces the winner if one reaches 5 points.     - Inputs from users are now in the form of 3 buttons labeled       'rock', 'paper', 'scissor'.     - All announcements of current score and winner are now displayed       on the browser instead of console log.

--- RPS.js
+++ RPS.js
@@ -4,29 +4,11 @@
 /* Dictionary that assigns number to rps strings
    If a subtraction between 2 keys is 1 or -2, it's a 
    winning condition, -1 or 2 are losing condition */
-const dict = { "paper": 0, "scissor": 1, "rock": 2,
-               0: "paper", 1: "scissor", 2: "rock" };
-
-
-
-// ============================================================================
-// This function asks the user if they wants to play a game of rock paper 
-// scissor. Game() is called once it receives a confirmation
-//     It takes no arguement
-//     Returns none
-// ============================================================================
-function Prompter() {
-    /* Ask the player to play a game. Exit if player doesn't play */
-    if (window.confirm("Do you want to play rock paper scissor?")) {
-        Game();
-    } else {
-        window.alert("Game not excecuted. Thank you");
-        console.log("Game is terminated");
-        return;
-    }
-} // End of func
-
-
+const dict = { 'paper': 0, 'scissor': 1, 'rock': 2,
+               0: 'paper', 1: 'scissor', 2: 'rock' };
+// Player and Computer score
+let playerScore = 0;
+let compScore = 0;
 
 // ============================================================================
 // This function plays the game out for 5 rounds and announces results on the  
@@ -34,17 +16,13 @@ function Prompter() {
 //     It takes no arguement
 //     Returns none
 // ============================================================================
-function Game() {
-    // Keep score on player and computer
-    let playerScore = 0;
-    let compScore = 0;
-
-    /* Iterate 5 times */
-    for (let i = 0; i < 5; i++) {
+function PlayRound(player) {
+    /* Iterate as long as neither player has reached score of 5 */
+    if(playerScore < 3 && compScore < 3) {
         /* Try catch block for error handling */
         try {
             // Call playRound()   
-            switch(playRound(playerPlay(), computerPlay())) {
+            switch(RoundCalc(player, computerPlay())) {
                 case 1:
                     // append player's score if the player wins,
                     playerScore += 1;
@@ -56,24 +34,22 @@ function Game() {
                 default:
                     break;
             }
+            // Anouncing current score
+            writeToDoc('Player:   ' + playerScore); 
+            writeToDoc('Computer: ' + compScore);
         } catch(err) {
-            console.log("The game is terminated");
+            console.log('The game is terminated');
             return;
         }
     }
 
-    // Anouncing total score
-    console.log("Player's score:   " + playerScore + 
-                "\nComputer's score: " + compScore);
-    // Announcing the winner, or if it's a draw
-    if(playerScore == compScore) {
-        console.log("It's a draw!");
-    } else {
-        let x = playerScore > compScore ? "player":"Computer";
-        console.log(x + " wins the game");
-    }
+    if(playerScore == 3 || compScore == 3) {
+        // Announcing the winner
+        let x = playerScore > compScore ? 'Player':'Computer';
+        writeToDoc('The winner is ' + x);
+        return;
+    }  
 } // End of func
-
 
 
 // ============================================================================
@@ -82,35 +58,32 @@ function Game() {
 //     It takes 2 integers as arguement
 //     Returns 0 if draw or invalid input, 1 if payer wins, 2 if computer wins.
 // ============================================================================
-function playRound(playerNum, computerNum) { 
+function RoundCalc(playerNum, computerNum) { 
     // Variables
-    let computer = dict[computerNum];       // string of computer's result
-    let player = dict[playerNum];           // string of player's result
+    let comp = dict[computerNum];       // string of computer's result
+    let pl = dict[playerNum];           // string of player's result
     let result = playerNum - computerNum;   // math. Get the result
-    switch (result) {
+    
+    switch(result) {
         // Stalemate condition
         case 0:
-            console.log("Both are " + computer + 
-                         ". This round is a draw!");
+            writeToDoc('Both are ' + comp + '. This round is a draw!');
             return 0;
         // Conditions where the player wins
         case 1:
         case -2:
-            console.log("player wins this round. " + 
-                         player + " beats " + computer + ".");
+            writeToDoc('player wins this round. ' + pl + ' beats ' + comp + '.');
             return 1;
         // Conditions where computer wins
         case -1:
         case 2:
-            console.log("computer wins this round. " + 
-                         computer + " beats " + player + ".");
+            writeToDoc('computer wins this round. ' + comp + ' beats ' + pl + '.');
             return 2;
         // Default. Throw an error 
         default:
             throw "Error calculating.";
     }   
 } // End of func
-
 
 
 // ============================================================================
@@ -126,43 +99,25 @@ function computerPlay() {
 } // End of func
 
 
-
 // ============================================================================
-// This function prompts the user to play the game and gets the input. It loops
-// if the input is not appropriate
-//     It takes no arguement
-//     Returns an integer that relfects user's input.
+//
 // ============================================================================
-function playerPlay() {
-    // Prompt player for value
-    let player = (window.prompt("Rock, paper, or scissor?")).toLowerCase();
-
-    /* If the answer is not in dict, Loop until 
-       the answer is right or function terminated */
-    while (!(player in dict)) {
-        // If the player hits cancel or gives null answer
-        if(!player) {
-            // Recursively calling the function if the player still wants to play
-            if (window.confirm(
-                "Do you still want to play?\nOK to play, Cancel to exit")){
-                return playerPlay();
-             // Throws an error otherwise
-            } else {
-                window.alert("The game is stopped. Thank you");
-                throw "The game is stopped.";
-            }
-        }
-        // Prompt for answer again otherwise
-        player = (window.prompt(player + " is not valid. Please type again")).toLowerCase();
-    }
-    // Return the dictionary key
-    window.alert("Your answer was " + player);
-    return dict[player];
+function writeToDoc(str) {
+    let container = document.getElementById('announce');
+    let score = document.createElement('p');
+    score.innerHTML = str;
+    container.appendChild(score);
 } // End of func
-
 
 
 // ============================================================================
 // Calling the game
 // ============================================================================
-Prompter();
+/* Only run the game if neither player has reached 5 points */
+const  ans = document.querySelectorAll("button");
+ans.forEach(button => button.addEventListener("click", function() {
+    if(playerScore < 3 && compScore < 3) {
+        let player = this.getAttribute("name");
+        PlayRound(dict[player]);
+    }
+}));
